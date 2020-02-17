82a0e09cbd87862e2d63c4cc0ecf73a0172a750a
lacey-crenshaw <lacey@laceycrenshaw.com>
2020-02-17 09:28:04 -0700

    commit on master

---
+++ webProgrammingDev/projects/digitalBusinessCard/card.js
@@ -0,0 +1,13 @@
+let card = {
+    title:"Web Developer",
+    email:"lacey@laceycrenshaw.com",
+    myFunc:function(){
+        return Date.now()
+    }
+}
+
+function card(title) {
+    this.title=title; 
+}
+
+let title2 = new title ("Project Manager"); 
